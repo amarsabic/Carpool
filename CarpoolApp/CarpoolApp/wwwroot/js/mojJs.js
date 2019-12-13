2daee2d198a76195410dc0f35dd2de5ec97a23ea
Amar Šabić <amar.sabic@edu.fit.ba>
2019-12-13 16:13:26 +0100

    Reduzajn homePage-a

---
+++ CarpoolApp/CarpoolApp/wwwroot/js/mojJs.js
@@ -0,0 +1,18 @@
+﻿/*TYPING EFFECT START*/
+
+var i = 0,
+    text;
+text = 'Ponudi. Pronađi. Vozi se.';
+
+
+function typingEffect() {
+    if (i < text.length) {
+        document.getElementById('naslovPretrazi').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typingEffect, 70);
+    }
+}
+
+typingEffect();
+
+/*TYPING EFFECT END*/
