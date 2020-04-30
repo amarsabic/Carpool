3b57d1dcc59a4574204d8d77a0ebb7582b89f3ed
Amar Šabić <amar.sabic@edu.fit.ba>
2020-04-30 05:42:03 +0200

    popravak cshtml (email, register..)

---
+++ CarpoolApp/CarpoolApp/Areas/Driver/Controllers/RezervacijeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarpoolApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpoolApp.Areas.Driver.Controllers
+{
+    public class RezervacijeController : BaseController
+    {
+        public RezervacijeController(CarpoolAppContext db) : base(db)
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
